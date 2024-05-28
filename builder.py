2ad787cc8e0615a4a106a8224ec4772ad0cdbb27
retr0 <mzchris97@gmail.com>
2024-05-27 19:27:33 -0700

    adding code...

--- builder.py
+++ builder.py
@@ -14,22 +14,34 @@ window.iconbitmap('C:\\Users\\retr0\\Documents\\python3\\resources\\1492608046-7
 
 global_var_options = False
 global_var_filename = ''
+global_var_name = ''
+global_var_windowed = False
+global_var_icon = False
 
 def toggle_options():
     global global_var_options
-    if global_var_options:
-        global_var_options = False
-    else:
-        global_var_options = True
+    global_var_options = False if global_var_options else True
+
+def toggle_windowed():
+    global global_var_windowed
+    global_var_windowed = False if global_var_windowed else True
 
 def build_options():
     toggle_options()
     if global_var_options:
         options_frame.pack(expand=True, fill='both', padx=5, pady=5)
 
+def askfileicon():
+    global global_var_icon
+    global_var_icon = filedialog.askopenfilename()
+
 def get_path():
     global global_var_filename
     global_var_filename = filedialog.askopenfilename()
+
+def init_build():
+    build_app.configure(text="Building...")
+    build_executable(global_var_name, True, global_var_windowed, global_var_icon)
 
 frame1 = ctk.CTkFrame(window)
 frame1.pack(expand=True, fill='both', padx=5, pady=5)
@@ -37,7 +49,7 @@ frame1.pack(expand=True, fill='both', padx=5, pady=5)
 load_app = ctk.CTkButton(frame1, text='Select File', command=get_path)
 load_app.pack(pady=20)
 
-build_app = ctk.CTkButton(frame1, text='Build Executable', fg_color='#e63746', hover_color='black')
+build_app = ctk.CTkButton(frame1, text='Build Executable', fg_color='#e63746', hover_color='black', command=init_build)
 build_app.pack()
 
 switch = ctk.CTkSwitch(frame1, text='custom build', command=build_options)
@@ -50,5 +62,14 @@ label = ctk.CTkLabel(subframe, text='Toggle custom build for icon, name, and run
 label.pack(side='left')
 
 options_frame = ctk.CTkFrame(frame1)
+
+entry = ctk.CTkEntry(options_frame, textvariable=global_var_name, placeholder_text="name of application")
+entry.pack(padx=5, pady=5, fill='x')
+
+windowed = ctk.CTkSwitch(options_frame, text="Windowed", command=toggle_windowed)
+windowed.pack(padx=5, pady=5, fill='x')
+
+select_path = ctk.CTkButton(options_frame, text="Select Icon", command=askfileicon)
+select_path.pack()
 
 window.mainloop()
